b60398ab18743f9fa7278106381680954ef08f00
BILLOT.CHERRY PICK <vbillot@tf1.fr>
2022-04-13 14:27:52 +0200

    init

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿namespace VendingMachine
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {ID} - {Name} - {Price:0.##} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VendingMachine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {  
+            VendingMachine vendingMachine = new VendingMachine();
+            vendingMachine.Start();
+        }
+    }
+}
